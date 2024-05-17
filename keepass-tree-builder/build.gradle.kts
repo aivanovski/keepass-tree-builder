8f481d1651311ffad64b9da21f068bd148f9353a
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2024-05-17 18:58:51 +0200

    Update version to 0.2.0

--- keepass-tree-builder/build.gradle.kts
+++ keepass-tree-builder/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 val appGroupId = "com.github.aivanovski"
 val appArtifactId = "keepass-tree-builder"
-val appVersion = "0.1.0"
+val appVersion = "0.2.0"
 
 group = appGroupId
 version = appVersion
